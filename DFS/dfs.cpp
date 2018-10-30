3ff4e45bb1f48651ed595aeeaba0e9017f831ea8
21171-somesh <33005692+21171-somesh@users.noreply.github.com>
2018-10-30 19:43:21 +0530

    update dfs

--- DFS/dfs.cpp
+++ DFS/dfs.cpp
@@ -1,19 +1,19 @@
-void dfsUtil(int visited[],int pos)
+void dfsUtil(int visited[],int pos, vector<vector<int>> adj)
 {
-	visited[pos]=1;
+	visited[pos] = 1;
 	cout<<pos<<endl;
-	for(auto it=adj[pos].begin();it!=adj[pos].end();it++)
+	for(auto it = adj[pos].begin(); it != adj[pos].end(); it++)
 	{
-		if(visited[*it]==0)
-			dfsUtil(visited,*it);
+		if(visited[*it] == 0)
+			dfsUtil(visited, *it, adj);
 	}
 }
-void dfs()
+void dfs(vector<vector<int>> adj)
 {
-	int visited[size]={0};
-	for(int i=0;i<size;i++)
+	int visited[adj.size()] = {0};
+	for(int i = 0; i < size; i++)
 	{
-		if(visited[i]==0)
-			dfsUtil(visited,i);
+		if(visited[i] == 0)
+			dfsUtil(visited, i, adj);
 	}
 }
